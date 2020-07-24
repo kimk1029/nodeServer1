7cc94386719203d212604e14c44ee27f78cdea27
김 규현 <kimk1029@Neowiz.com>
2020-07-24 11:26:46 +0900

    수정 테스트

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const HOST = "0.0.0.0";
 const app = express();
 app.get("/", (req, res) => {
 	res.send("Hello world\n");
-	res.send("Hello world@@@@@@@@@@@@@@@@@\n");
+	res.send("Hello world@@@@@@34232343@@@@@@@@@@@\n");
 	console.log(`Running`);
 });
 app.listen(PORT, HOST);
